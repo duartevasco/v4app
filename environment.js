8ee50426b23f782caa15ddd1713fb3b89f2ce94b
MarkNijhof <Mark.Nijhof@cre8ivethought.com>
2012-10-22 15:55:00 +0200

    added environment.js file

---
+++ environment.js
@@ -0,0 +1,32 @@
+
+exports.variables = function(node_env) {
+	var environment = node_env || process.env.NODE_ENV
+	
+	var port = process.env.PORT
+	
+	if (environment.toLowerCase() === 'development') {
+		return {
+			NODE_ENV: environment,
+			PORT: port,
+			MYSQL_USER: process.env.USER,
+			MYSQL_DATABASE: 'stats',
+			MYSQL_HOST: 'localhost'
+		}
+	}
+	if (environment.toLowerCase() === 'production') {
+		return {
+			NODE_ENV: environment,
+			PORT: port
+			MYSQL_USER: '',
+			MYSQL_DATABASE: 'stats',
+			MYSQL_HOST: ''
+		}
+	}
+	return {
+		NODE_ENV: '',
+		PORT: 0
+		MYSQL_USER: '',
+		MYSQL_DATABASE: '',
+		MYSQL_HOST: ''
+	}
+}
