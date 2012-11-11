c5d1f72cb5ad5d7ae5d0baf2e1c308f10689b9ba
Vasco Duarte <duarte_vasco@yahoo.com>
2012-11-11 12:02:09 +0100

    made the get_all_weeks_in_array() more readable by extracting magic substrings

--- routes/get_weeks_v2.js
+++ routes/get_weeks_v2.js
@@ -1,16 +1,28 @@
 var process_env = require('../environment').variables();
 var db = require('../mysql_connector').new_db();
 
+var extract_week_from_table_name = function( table_name ) {
+	return table_name.substring(10, 12)
+}
+
+var extract_year_from_table_name = function( table_name ) {
+	return table_name.substring(6, 10)
+} 
+
+var build_title_and_link_to_display = function( week, year ) {
+	return {
+		href: '/get/total/count/'+ year +'/'+ week,
+		title: 'totals for year '+ year +' and week '+ week
+	}
+}
+
 var get_all_weeks_in_array = function(rows){
 	var weeks = new Array()
 	for (var i in rows) {
 		var table_name = rows[i].Tables_in_stats
-		var week = table_name.substring(10, 12)
-		var year = table_name.substring(6, 10)
-		weeks.push({
-			href: '/get/total/count/'+ year +'/'+ week,
-			title: 'totals for year '+ year +' and week '+ week
-		})
+		var week = extract_week_from_table_name( table_name )
+		var year = extract_year_from_table_name( table_name )
+		weeks.push( build_title_and_link_to_display( week, year ) )
 	}
 	return weeks
 }
